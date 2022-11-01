80344bc5d3303013ac5f7cfe0c3c018b835bc20b
chickenchickenlove <ojt90902@naver.com>
2022-11-01 23:05:07 +0900

    chapter5 retry

---
+++ src/main/java/kafkaStreams/chapter5_test/TumblingWindowStreamRetry.java
@@ -0,0 +1,60 @@
+package kafkaStreams.chapter5_test;
+
+import kafkaStreams.chapter3.GsonDeserializer;
+import kafkaStreams.chapter3.GsonSerializer;
+import kafkaStreams.domain.StockTransaction;
+import kafkaStreams.domain.TransactionSummary;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.*;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.UUID;
+
+import static org.apache.kafka.streams.Topology.AutoOffsetReset.EARLIEST;
+
+public class TumblingWindowStreamRetry {
+
+    public static final String STOCK_TRANSACTIONS_TOPIC = "stock-transactions";
+
+    public static void main(String[] args) {
+
+
+        GsonSerializer<StockTransaction> stockTransactionGsonSerializer = new GsonSerializer<>();
+        GsonDeserializer<StockTransaction> stockTransactionGsonDeserializer = new GsonDeserializer<>(StockTransaction.class);
+
+        GsonSerializer<TransactionSummary> transactionSummaryGsonSerializer = new GsonSerializer<>();
+        GsonDeserializer<TransactionSummary> transactionSummaryGsonDeserializer = new GsonDeserializer<>(TransactionSummary.class);
+
+        Serde<String> stringSerde = Serdes.String();
+        Serde<StockTransaction> stockTransactionSerde = Serdes.serdeFrom(stockTransactionGsonSerializer, stockTransactionGsonDeserializer);
+        Serde<TransactionSummary> transactionSummarySerde = Serdes.serdeFrom(transactionSummaryGsonSerializer, transactionSummaryGsonDeserializer);
+
+
+        Properties props = new Properties();
+
+        props.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, UUID.randomUUID().toString());
+
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KTable<Windowed<TransactionSummary>, Long> countStream = builder.stream(STOCK_TRANSACTIONS_TOPIC,
+                        Consumed.with(stringSerde, stockTransactionSerde)
+                                .withOffsetResetPolicy(EARLIEST))
+                .groupBy((key, stockTransaction) -> TransactionSummary.from(stockTransaction),
+                        Grouped.with(transactionSummarySerde, stockTransactionSerde))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(20)))
+                .count();
+
+
+        countStream.toStream().peek((key, value) -> System.out.println("key = " + key + " value = " + value));
+
+        KafkaStreams kafkaStreams = new KafkaStreams(builder.build(), props);
+        kafkaStreams.start();
+    }
+}
